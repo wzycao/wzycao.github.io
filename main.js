bc9b52b761d1fe2439ef5ec0a0c44205889c18d9
William <wzycao@gmail.com>
2025-12-01 22:05:02 -0600

    initial scene setup

---
+++ main.js
@@ -0,0 +1,25 @@
+import * as T from './libs/CS559-Three/build/three.module.js';
+
+const scene = new T.Scene();
+const camera = new T.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+
+const renderer = new T.WebGLRenderer();
+renderer.setSize( window.innerWidth, window.innerHeight );
+renderer.setAnimationLoop( animate );
+document.body.appendChild( renderer.domElement );
+
+const geometry = new T.BoxGeometry( 1, 1, 1 );
+const material = new T.MeshBasicMaterial( { color: 0x00ff00 } );
+const cube = new T.Mesh( geometry, material );
+scene.add( cube );
+
+camera.position.z = 5;
+
+function animate() {
+
+  cube.rotation.x += 0.01;
+  cube.rotation.y += 0.01;
+
+  renderer.render( scene, camera );
+
+}
