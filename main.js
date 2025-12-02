b3c0c79c559a78bbf42a4d838cb3fc73b6a08c19
William <wzycao@gmail.com>
2025-12-01 22:48:15 -0600

    event listener for resizing window

--- main.js
+++ main.js
@@ -23,3 +23,15 @@ function animate() {
   renderer.render( scene, camera );
 
 }
+
+
+/* event listeners
+ *
+ *
+ * 
+ */
+window.addEventListener("resize", () => { 
+    camera.aspect = window.innerWidth / window.innerHeight; 
+    camera.updateProjectionMatrix();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+})
