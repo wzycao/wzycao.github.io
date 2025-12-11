cfd810cfa7354a32187b5557af9a3624556db9f3
William <wzycao@gmail.com>
2025-12-11 15:05:11 -0600

    added basic UI with one functioning element. added bounding boxes for player object (to use these as bounding boxes, make them invisible and apply a Box3 onto them.

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 import * as T from './libs/CS559-Three/build/three.module.js';
 import { OBJLoader } from './libs/CS559-Three/examples/jsm/loaders/OBJLoader.js';
+import { OrbitControls } from "../libs/CS559-Three/examples/jsm/controls/OrbitControls.js";
 
 
 /**
@@ -10,6 +11,7 @@ import { OBJLoader } from './libs/CS559-Three/examples/jsm/loaders/OBJLoader.js'
 let sceneActive = false;
 let optionsMenu = false;
 document.getElementById('controls-menu').style.display = 'none';
+document.getElementById('ui').style.display = 'none';
 
 let upKey = "w";
 let downKey = "s";
@@ -19,6 +21,11 @@ let rightKey = "d";
 let accelKey = "q";
 let decelKey = "e";
 
+
+/**
+ * Display variables
+ */
+const speedUI = document.querySelector("#speed");
 
 
 const controls = {
@@ -55,6 +62,9 @@ window.addEventListener("keyup", (e) => {
  * 
  */
 const scene = new T.Scene();
+
+
+
 const camera = new T.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
 const renderer = new T.WebGLRenderer();
@@ -65,7 +75,10 @@ scene.add(gridHelper);
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
-
+//Debugging camera
+const debug_camera = new T.PerspectiveCamera();
+let debug_controls = new OrbitControls(debug_camera, renderer.domElement);
+scene.add(debug_camera);
 //Main light source 
 const light = new T.HemisphereLight( 0xffffbb, 0x080820, 1 );
 scene.add(light);
@@ -77,13 +90,25 @@ scene.add(light);
 //Objects
 
 //Replace cube with either a collision box or point (which will be used to generate other stuff like the model)
-const geometry = new T.BoxGeometry( 1, 1, 1 );
-const material = new T.MeshBasicMaterial( { color: 0x00ff00 } );
+const geometry = new T.BoxGeometry( 0.1, 0.1, 0.1 );
+const material = new T.MeshStandardMaterial();
 const cube = new T.Mesh( geometry, material );
 const axesHelper = new T.AxesHelper(4); 
 cube.add(axesHelper);
 scene.add( cube );
 cube.add(camera);
+
+
+//Bounding Box 1
+const geometry1 = new T.BoxGeometry( 4, 4, 5 );
+const wireframeMat = new T.MeshStandardMaterial({wireframe:true});
+const cube1 = new T.Mesh( geometry1, wireframeMat );
+cube.add(cube1);
+
+//Bounding Box 2
+const geometry2 = new T.BoxGeometry( 2, 2, 4.5 );
+const cube2 = new T.Mesh( geometry2, wireframeMat );
+cube1.add(cube2);
 
 
 camera.position.set(0,5,10);
@@ -105,7 +130,7 @@ main_man.traverse(obj => {
     });
 
 main_man.scale.set(0.25,0.25,0.25);
-main_man.position.set(0,-1,3);
+main_man.position.set(0,-0.8,3);
 main_man.rotation.set(2,0,3.13);
 cube.add(main_man);
 
@@ -115,11 +140,14 @@ let rotVel = 0;
 let upVel = 0;
 let charRotX = 0;
 let charRotZ = 0;
+let charRotY = 0;
 
 let rotSpeed = 0.001;
 let upSpeed = 0.015;
 let fwdSpeed = -0.09;
+let dispSpeed = 0;
 
+renderer.render(scene, debug_camera);
 
 /**
  * Main animation loop
@@ -175,15 +203,26 @@ function animate() {
   // remember to change the multipliers here
   
   charRotX = 2*upVel;
+  charRotY = 50*rotVel;
   charRotZ = 10*rotVel;
-  main_man.rotation.set(1.8 + charRotX,0,3.13 - charRotZ);
+  main_man.rotation.set(1.8 + charRotX,0 + charRotY,3.13 - charRotZ );
+  cube1.rotation.set( charRotX,0.5*charRotY,2*charRotZ)
 
 
-  cube.translateZ(fwdSpeed);
-  renderer.render( scene, camera);
+  // comment out translateZ here for debug
+  //cube.translateZ(fwdSpeed);
 
+
+  //Display current speed as a rounded integer
+  //Here, we could think of using the math.floor thing to instead display something fancier than just a number (a bar?)
+  dispSpeed = Math.floor(-7*fwdSpeed);
+
+  speedUI.textContent = dispSpeed;
+  renderer.render(scene, debug_camera);
   requestAnimationFrame(animate);
 }
+// place requestAnimationFrame(animate); here for easy debug (graphics will always be showing)
+requestAnimationFrame(animate);
 
 
 
@@ -196,6 +235,7 @@ document.getElementById('start').addEventListener('click', () => {
         if (!sceneActive) {
             requestAnimationFrame(animate);
             document.getElementById('main-menu').style.display = 'none';
+            document.getElementById('ui').style.display = 'inline-flex';
             sceneActive = true;
         }
 });
